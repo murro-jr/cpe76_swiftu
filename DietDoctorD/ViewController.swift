51b9977be5acbe9ae7d175c549e2170707aaa954
June Hermoso <junehermoso@junhermoso.local>
2018-12-09 17:27:26 +0800

    Initial Commit

---
+++ DietDoctorD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DietDoctorD
+//
+//  Created by June Hermoso on 09/12/2018.
+//  Copyright © 2018 JH Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
